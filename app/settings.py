f69e6fb6783a1d30ac2d4a61c347526967a26150
duhow <contacto@duhowpi.net>
2026-09-17 16:28:03 +0200

    fix: update path for local sqlite db

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///data/app.db"
+    database_url: str = "sqlite:////data/app.db"
 
     model_config = SettingsConfigDict(
         env_file=".env",
